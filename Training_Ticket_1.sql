a4a0fbcab8b5c2eb0e3440c0a730ed5868e3dee8
anaisjouenne <61699595+anaisjouenne@users.noreply.github.com>
2020-04-14 16:33:50 +0200

    Create Training_Ticket_1.sql
    Ticket Training 1 Q1 to Q12

---
+++ Training_Ticket_1.sql
@@ -0,0 +1,24 @@
+--This is a SQL training ticket, it contains requirements of increasing difficulty to get more familiar with SQL and our data.
+-- For each of the required elements, create a SQL file.
+
+--You find data regarding companies in our object layer:
+--SELECT * FROM ol.salesforce_accounts LIMIT 10
+--The honeypot_it correlates with the company_id of other tables.
+--The owner_id connects with ol.salesforce_users and gives information about who the current CSM is.
+--The type column is an indicator if the company is still an active client or maybe just a lead.
+--Those are clients we would consider as clients (or former clients): ('Customer','Reactivation','Cancelled Client','Closed Down','Do Not Contact')
+--You can find data about placements in placements
+--You can find our time dimension in time_d
+
+
+
+
+
+
+
+
+
+
+
+
+
